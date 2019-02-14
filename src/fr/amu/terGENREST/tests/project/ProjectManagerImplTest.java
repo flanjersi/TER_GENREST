09546f30bdf847de715e0cc7863e6095baa982cc
ramzi-guellil <brixyes@gmail.com>
2019-02-14 14:03:00 +0100

    ajout d'entité projet avec test

--- src/fr/amu/terGENREST/tests/project/ProjectManagerImplTest.java
+++ src/fr/amu/terGENREST/tests/project/ProjectManagerImplTest.java
@@ -1,18 +1,11 @@
 package fr.amu.terGENREST.tests.project;
-
-import static org.junit.Assert.*;
-
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
 import javax.ejb.embeddable.EJBContainer;
 import javax.transaction.Transactional;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-
 import fr.amu.terGENREST.services.project.ProjectManager;
 import fr.amu.terGENREST.services.user.UserManager;
 import fr.amu.terGENREST.entities.project.Project;
@@ -21,7 +14,6 @@ import fr.amu.terGENREST.entities.user.User;
 
 @Transactional
 public class ProjectManagerImplTest {
-
 
 	@EJB
 	ProjectManager projectManager;
@@ -40,77 +32,54 @@ public class ProjectManagerImplTest {
 		EJBContainer.createEJBContainer().close();
 	}
 
-
-	@Ignore
 	@Test
 	public void testFindProject() {
-
 		Project project = new Project("firstProject");
 		Project project2 = new Project("secondProject");
-
 		projectManager.addProject(project);
 		projectManager.addProject(project2);
-
-
 	}
-@Ignore
+
 	@Test
 	public void testAddProject() {
-		
-		User user = new User("firstName", "lastName", "email@email.com", "password");
-		
+		User user = new User("firstName", "lastName", "email0@email.com", "password");
 		Project project = new Project("firstProject");
-		
 		user.addProject(project);
-		
 		userManager.saveUser(user);
-		
 		Project projcetAdded = projectManager.findProject(project.getId()); 
-
-		Assert.assertEquals(projcetAdded, project);
-		
+		Assert.assertEquals(projcetAdded, project);	
+		userManager.removeUser(user);
 	}
 
-@Ignore
 	@Test
 	public void testRemoveProject() {
-
 		User user = new User("firstName", "lastName", "email@email.com", "password");
-		
-		Project project = new Project("firstProject");
-		
+		Project project = new Project("firstProject");		
 		user.addProject(project);
-		
 		userManager.saveUser(user);
-		
 		user.removeProject(project);
-		
 		userManager.updateUser(user);
-		
 		Project projcetAdded = projectManager.findProject(project.getId()); 
-
 		Assert.assertNull(projcetAdded);
-
+		userManager.removeUser(user);
 	}
 
-@Ignore
 	@Test
-	public void testUpdateProject() {
-		
-		User user = new User("firstName", "lastName", "email@email.com", "password");
+	public void testUpdateProject() {	
+		User user = new User("firstName", "lastName", "email1@email.com", "password");
 		Project project = new Project("firstProject");
 		user.addProject(project);
 		userManager.saveUser(user);
 		project.setProjectName("secondProject");
 		userManager.updateUser(user);
 		Assert.assertEquals("secondProject", projectManager.findProject(project.getId()).getProjectName());
+		userManager.removeUser(user);
 	}
 	
-
 	@Test
 	public void testFindAllProject() {
-		User user = new User("firstName", "lastName", "email@email.com", "password");
-		User user2 = new User("firstName", "lastName", "email@email.com", "password");
+		User user = new User("firstName", "lastName", "email2@email.com", "password");
+		User user2 = new User("firstName", "lastName", "email22@email.com", "password");
 		Project project = new Project("firstProject");
 		Project project2 = new Project("firstProject");
 		Project project3 = new Project("firstProject");
@@ -119,8 +88,9 @@ public class ProjectManagerImplTest {
 		user2.addProject(project3);
 		userManager.saveUser(user);
 		userManager.saveUser(user2);
-		
-		Assert.assertEquals(3,projectManager.findAllProject().size());
+		Assert.assertEquals(3,projectManager.findAllProject().size());		
+		userManager.removeUser(user);
+		userManager.removeUser(user2);
 	}
 
 
