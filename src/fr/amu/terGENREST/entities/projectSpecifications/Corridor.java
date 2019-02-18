e968939884478f53d313b294ead27040bfbee9cd
flanjersi <grosj83670@gmail.com>
2019-02-18 23:35:45 +0100

    Update Corridor.java

--- src/fr/amu/terGENREST/entities/projectSpecifications/Corridor.java
+++ src/fr/amu/terGENREST/entities/projectSpecifications/Corridor.java
@@ -105,7 +105,41 @@ public class Corridor implements Serializable {
 		this.actuators = actuators;
 	}
 
+	@Override
+	public String toString() {
+		return "Corridor [id=" + id + ", numberCorridor=" + numberCorridor + ", sensors=" + sensors + ", actuators="
+				+ actuators + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + numberCorridor;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Corridor other = (Corridor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (numberCorridor != other.numberCorridor)
+			return false;
+		return true;
+	}
 
 
+	
 	
 }
