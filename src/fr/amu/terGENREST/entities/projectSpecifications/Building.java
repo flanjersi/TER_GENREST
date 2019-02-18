2602dc6aa0ba43a3a4c6ef02f3bdab42693ed6b3
flanjersi <grosj83670@gmail.com>
2019-02-18 23:35:10 +0100

    Update Building.java

--- src/fr/amu/terGENREST/entities/projectSpecifications/Building.java
+++ src/fr/amu/terGENREST/entities/projectSpecifications/Building.java
@@ -75,4 +75,44 @@ public class Building implements Serializable {
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+
+	@Override
+	public String toString() {
+		return "Building [id=" + id + ", address=" + address + ", buildingFloor=" + buildingFloor + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Building other = (Building) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
