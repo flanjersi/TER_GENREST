5b90f59faa6543d275bbd8b3ba98a1762d80edb2
flanjersi <grosj83670@gmail.com>
2019-02-18 23:41:17 +0100

    Update DataManager.java

--- src/fr/amu/terGENREST/services/projectSpecifications/DataManager.java
+++ src/fr/amu/terGENREST/services/projectSpecifications/DataManager.java
@@ -10,10 +10,13 @@ import fr.amu.terGENREST.entities.projectSpecifications.Data;
 
 public interface DataManager {
 
+
+	@Deprecated
 	public void addData(Data data);
 	
 	public Data updateData(Data data);
 
+	@Deprecated
 	public void removeData(Data data);
 
 	public Data findData(Long id);
