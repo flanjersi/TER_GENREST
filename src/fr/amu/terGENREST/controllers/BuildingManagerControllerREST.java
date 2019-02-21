e3125bf02bc3a6fd0896009bc6318f0e7be01c5b
ramzi-guellil <brixyes@gmail.com>
2019-02-21 12:10:55 +0100

    pull

--- src/fr/amu/terGENREST/controllers/BuildingManagerControllerREST.java
+++ src/fr/amu/terGENREST/controllers/BuildingManagerControllerREST.java
@@ -1,10 +1,5 @@
 package fr.amu.terGENREST.controllers;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -22,18 +17,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-<<<<<<< HEAD
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.junit.After;
-import org.junit.Before;
-
-=======
->>>>>>> a38c21e19997bda637ce4e05c23be1d1b058a60d
 import fr.amu.terGENREST.controllers.utils.Utils;
 import fr.amu.terGENREST.entities.projectSpecifications.Building;
 import fr.amu.terGENREST.entities.projectSpecifications.Floor;
@@ -60,12 +43,6 @@ public class BuildingManagerControllerREST {
 	@EJB
 	private UserManager userManager;
 
-<<<<<<< HEAD
-	
-=======
-
-
->>>>>>> a38c21e19997bda637ce4e05c23be1d1b058a60d
 	@GET
 	@Path("")
 	public Response getAllBuildings() {
@@ -89,93 +66,6 @@ public class BuildingManagerControllerREST {
 		return Response.ok().entity(building).build();
 	}
 
-
-<<<<<<< HEAD
-	@PUT
-	@Path("")
-	public Response createBuilding(@PathParam("idProject") Long idProject, Building building,@PathParam("idUser") Long idUser) {
-		
-
-		if(userManager.findUser(idUser) == null) {
-			return Response
-					.status(404)
-					.entity(Utils.makeErrorMessage(400, "User with id '" + idUser + "' no exist"))
-					.build();
-		}
-		List<Project> projects = userManager.findUser(idUser).getProjects();
-		
-		long nbProject = projects
-				.stream()
-				.filter(projectUser -> (projectUser.getId().equals(idProject)))
-				.count();
-		
-		if(nbProject == 0) {
-			return Response
-					.status(404)
-					.entity(Utils.makeErrorMessage(400, "Project with id '" + idProject + "' no exist"))
-					.build();
-		}
-			
-		if(building.getType() == null) {
-			return Response
-					.status(400)
-					.entity(Utils.makeErrorMessage(400, "'type' property is missing"))
-					.build();		
-		}
-		
-		Project p = projectManager.findProject(idProject);
-       
-		p.addBuilding(building);
-		
-		projectManager.updateProject(p);
-		
-		Optional<Building> buildingAdded = p.getBuilding()
-				.stream()
-				.filter(projectUser -> (projectUser.getType().equals(building.getType())))
-				.findFirst();
-		
-		JsonObject jsonResponse = Json.createObjectBuilder().add("id", buildingAdded.get().getId()).build();
-		return Response.ok().entity(jsonResponse).build();
-		}
-	
-	@POST
-	@Path("/{id:[0-9]+}")
-	public Response updateBuilding(@PathParam("id") Long id,@PathParam("idUser") Long idUser,@PathParam("idProject") Long idProject, Building buildingUpdated) {
-		
-		if(userManager.findUser(idUser) == null) {
-
-			return Response
-					.status(404)
-					.entity(Utils.makeErrorMessage(400, "User with id '" + idUser + "' no exist"))
-					.build();
-		}
-
-		
-		if(projectManager.findProject(idProject) == null) {
-			return Response
-					.status(404)
-					.entity(Utils.makeErrorMessage(400, "Project with id '" + idProject + "' no exist"))
-					.build();
-		}
-		
-
-		long nbBuilding = projectManager.findProject(idProject).getBuilding()
-				.stream()
-				.filter(buildingProject -> (buildingProject.getId() == buildingUpdated.getId()))
-				.count();
-		
-		if(nbBuilding == 0) {
-			return Response
-					.status(404)
-					.entity(Utils.makeErrorMessage(400, "Building with id '" + buildingUpdated.getId() + "' no exist"))
-					.build();
-		}		
-		Building building = buildingManager.updateBuilding(buildingUpdated);
-		List<Building> buildingsResearch = projectManager.findProject(idProject).getBuilding();		
-		Optional<Building> buildingResearch = buildingsResearch.stream().filter(buildingFinded -> buildingFinded.equals(buildingUpdated)).findFirst();
-				
-		return Response.status(200).entity(buildingResearch).build();
-=======
 	@POST
 	@Path("/{idBuilding:[0-9]+}")
 	public Response updateBuilding(@PathParam("idBuilding") Long idBuilding, Building building) {
@@ -201,7 +91,6 @@ public class BuildingManagerControllerREST {
 		buildingFinded = buildingManager.updateBuilding(buildingFinded);
 
 		return Response.ok().entity(buildingFinded).build();
->>>>>>> a38c21e19997bda637ce4e05c23be1d1b058a60d
 	}
 	
 
--- src/fr/amu/terGENREST/tests/endpointsWS/SensorRESTControllerTest.java
+++ src/fr/amu/terGENREST/tests/endpointsWS/SensorRESTControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import fr.amu.terGENREST.tests.utils.PayloadDataRequestREST;
+import fr.amu.terGENREST.tests.utils.RequestHelper;
 import fr.amu.terGENREST.tests.utils.RequestsHelper;
 
 public class SensorRESTControllerTest {
@@ -14,48 +14,49 @@ public class SensorRESTControllerTest {
 
 	@Before
 	public void setUp() throws IOException {
+		
 		//ADD USER
-		RequestsHelper.ResponseJsonObject response = RequestsHelper.httpPUT("http://localhost:8090/api/users", PayloadDataRequestREST.jsonPayloadRequestUser());
+		RequestsHelper.ResponseJsonObject response = RequestsHelper.httpPUT("http://localhost:8090/api/users", RequestHelper.jsonPayloadRequestUser());
 		long idUser = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD PROJECT
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/users/" + idUser + "/projects", PayloadDataRequestREST.jsonPayloadRequestProject());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/users/" + idUser + "/projects", RequestHelper.jsonPayloadRequestProject());
 		long idProject = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD BUILDING
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/projects/" + idProject + "/buildings", PayloadDataRequestREST.jsonPayloadRequestBuilding());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/projects/" + idProject + "/buildings", RequestHelper.jsonPayloadRequestBuilding());
 		long idBuilding = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD FLOOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/buildings/" + idBuilding + "/floors", PayloadDataRequestREST.jsonPayloadRequestFloor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/buildings/" + idBuilding + "/floors", RequestHelper.jsonPayloadRequestFloor());
 		long idFloor = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD CORRIDOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/floors/" + idFloor + "/corridors", PayloadDataRequestREST.jsonPayloadRequestCorridor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/floors/" + idFloor + "/corridors", RequestHelper.jsonPayloadRequestCorridor());
 		long idCorridor = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD SENSOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/corridors/" + idCorridor + "/sensors", PayloadDataRequestREST.jsonPayloadRequestSensor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/corridors/" + idCorridor + "/sensors", RequestHelper.jsonPayloadRequestSensor());
 		long idSensor = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD MOTHER ROOM
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/floors/" + idFloor + "/motherRooms", PayloadDataRequestREST.jsonPayloadRequestMotherRoom());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/floors/" + idFloor + "/motherRooms", RequestHelper.jsonPayloadRequestMotherRoom());
 		long idMotherRoom = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD ROOM
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/motherRooms/" + idMotherRoom + "/rooms", PayloadDataRequestREST.jsonPayloadRequestRoom());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/motherRooms/" + idMotherRoom + "/rooms", RequestHelper.jsonPayloadRequestRoom());
 		long idRoom = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD SENSOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/rooms/" + idRoom + "/sensors", PayloadDataRequestREST.jsonPayloadRequestSensor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/rooms/" + idRoom + "/sensors", RequestHelper.jsonPayloadRequestSensor());
 		long idSensorInRoom = response.getPayload().getJsonNumber("id").longValue();
 
 		//ADD CORRIDOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/motherRooms/" + idMotherRoom + "/corridors", PayloadDataRequestREST.jsonPayloadRequestCorridor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/motherRooms/" + idMotherRoom + "/corridors", RequestHelper.jsonPayloadRequestCorridor());
 		long idCorridorInMotherRoom = response.getPayload().getJsonNumber("id").longValue();
 		
 		//ADD SENSOR
-		response = RequestsHelper.httpPUT("http://localhost:8090/api/corridors/" + idCorridorInMotherRoom + "/sensors", PayloadDataRequestREST.jsonPayloadRequestSensor());
+		response = RequestsHelper.httpPUT("http://localhost:8090/api/corridors/" + idCorridorInMotherRoom + "/sensors", RequestHelper.jsonPayloadRequestSensor());
 		long idSensorInCorridorInMotherRoom = response.getPayload().getJsonNumber("id").longValue();
 
 	}
